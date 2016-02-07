189ef3319acc3fd1402bd1ef17cadaa5f2720882
DavidChao99 <chaodavid99@gmail.com>
2016-02-07 11:44:10 -0500

    Final Commit @HackTJ

--- app/src/main/java/com/example/david/snowday/MainActivity.java
+++ app/src/main/java/com/example/david/snowday/MainActivity.java
@@ -3,11 +3,54 @@ package com.example.david.snowday;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.parse.Parse;
+import com.parse.ParseAnonymousUtils;
+import com.parse.ParseInstallation;
+import com.parse.ParseObject;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
+
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        Parse.initialize(this);
+        ParseInstallation.getCurrentInstallation().saveInBackground();
+
+        ParseUser.enableAutomaticUser();
+
+            if (ParseAnonymousUtils.isLinked(ParseUser.getCurrentUser())) {
+            Intent intent = new Intent(MainActivity.this,
+                    LoginSignUpActivity.class);
+            startActivity(intent);
+            finish();
+        } else {
+            ParseUser currentUser = ParseUser.getCurrentUser();
+            if (currentUser != null) {
+                Intent intent = new Intent(MainActivity.this, Welcome.class);
+                startActivity(intent);
+                finish();
+            } else {
+                Intent intent = new Intent(MainActivity.this,
+                        LoginSignUpActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }
+
     }
 }
